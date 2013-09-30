1458f2af5ac2d9121676be84c2ca90c37dc0d591
Josh Jordan <josh.jordan@gmail.com>
2013-09-30 09:48:21 -0400

    Note about logging

--- lib/airbadger/warning_suppression.rb
+++ lib/airbadger/warning_suppression.rb
@@ -1,5 +1,6 @@
 module Airbadger::WarningSuppression
   def without_warnings
+    #TODO: a better approach would be to log this to an Airbadger logger
     old_verbose, $VERBOSE = $VERBOSE, nil
     yield
     $VERBOSE = old_verbose
