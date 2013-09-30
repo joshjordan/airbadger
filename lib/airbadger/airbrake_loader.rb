ccbf023f9afaf3520b9008da49e679815e11faa8
Josh Jordan <josh.jordan@gmail.com>
2013-09-30 11:29:24 -0400

    Support multiple separate singletons

--- lib/airbadger/airbrake_loader.rb
+++ lib/airbadger/airbrake_loader.rb
@@ -2,5 +2,6 @@ class Airbadger::AirbrakeLoader
   def self.load_as(airbrake_alias)
     load 'airbrake.rb'
     Object.const_set(airbrake_alias, Airbrake)
+    Object.send(:remove_const, 'Airbrake')
   end
 end
--- spec/airbrake_loader_spec.rb
+++ spec/airbrake_loader_spec.rb
@@ -4,8 +4,11 @@ describe Airbadger::AirbrakeLoader do
   describe '.load_as' do
     it 'loads a new Airbrake singleton with the given name' do
       Airbadger::AirbrakeLoader.load_as('Errbit')
+      Airbadger::AirbrakeLoader.load_as('Raygun')
 
       Errbit.should respond_to :notify
+      Raygun.should respond_to :notify
+      Errbit.should_not eq Raygun
     end
   end
 end
