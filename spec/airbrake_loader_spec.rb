86074c75c279f2b2ec506c22e7c24d442930ea72
Josh Jordan <josh.jordan@gmail.com>
2013-10-01 00:02:04 -0400

    Test to prove preserve_namespaced_calls!

--- spec/airbrake_loader_spec.rb
+++ spec/airbrake_loader_spec.rb
@@ -6,13 +6,27 @@ describe Airbadger::AirbrakeLoader do
   describe '.load_as' do
     it 'loads a new Airbrake singleton with the given name' do
       without_warnings do
-        Airbadger::AirbrakeLoader.load_as('Errbit')
-        Airbadger::AirbrakeLoader.load_as('Raygun')
+        described_class.load_as('Errbit')
+        described_class.load_as('Raygun')
       end
 
       Errbit.should respond_to :notify
       Raygun.should respond_to :notify
       Errbit.should_not eq Raygun
+    end
+
+    it 'produces endpoints singletons capable of receiving error data' do
+      without_warnings do
+        described_class.load_as('Errbit')
+      end
+      Errbit.configure do |config|
+        config.test_mode = true
+      end
+
+      now = DateTime.now.to_s
+      Errbit.notify(Exception.new(now))
+
+      Errbit.sender.last_notice.should include(now)
     end
   end
 end
