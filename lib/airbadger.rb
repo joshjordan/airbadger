9d2b9ed818247f5449b2189ef2e86dcff821227d
Josh Jordan <josh.jordan@gmail.com>
2013-10-04 12:29:45 -0700

    Never allow uninitialized constant by creating the module right up front

--- lib/airbadger.rb
+++ lib/airbadger.rb
@@ -1,8 +1,8 @@
+require 'airbadger/version'
+
 require 'airbadger/warning_suppression'
 require 'airbadger/airbrake_loader'
 require 'airbadger/configuration'
-
-require 'airbadger/version'
 
 module Airbadger
   def self.configure(&block)
